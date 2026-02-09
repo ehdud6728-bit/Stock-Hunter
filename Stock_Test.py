f9bcce5d3e2e31d3b7aca41e534491828a28e00c
ehdud6728-bit <ehdud6728@gmail.com>
2026-02-09 16:53:52 +0900

    Stock_Test.py 업데이트

--- Stock_Test.py
+++ Stock_Test.py
@@ -68,8 +68,8 @@ def calculate_strategy_stats(all_hits):
 # =================================================
 # ⚙️ [1. 설정 및 글로벌 변수]
 # =================================================
-SCAN_DAYS = 30
-TOP_N = 3000 
+SCAN_DAYS = 10
+TOP_N = 350 
 START_DATE = (datetime.now() - timedelta(days=600)).strftime('%Y-%m-%d')
 END_DATE_STR = datetime.now().strftime('%Y%m%d')
 
