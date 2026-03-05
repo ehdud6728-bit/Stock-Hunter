e3f776e51fe52c1810978153cf8dfa323be030ca
ehdud6728-bit <ehdud6728@gmail.com>
2026-03-05 10:56:55 +0900

    debug_dante.py 업데이트

--- debug_dante.py
+++ debug_dante.py
@@ -75,7 +75,7 @@ def get_dynamic_analysis(stock_name, stock_code):
     return {
         'stock': stock_name,
         'score': score,
-        'themes': themes,
+        'themes': base_keywords,
         'grade': grade,
         'signals': signals,
         'keywords_used': base_keywords
