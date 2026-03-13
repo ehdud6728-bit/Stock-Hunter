0f4c09bf81f8c60245bd932d24040f5fbf1666a9
ehdud6728-bit <ehdud6728@gmail.com>
2026-03-13 17:24:50 +0900

    theme_stock_map.py 만들기

---
+++ theme_stock_map.py
@@ -0,0 +1,66 @@
+THEME_STOCK_MAP = {
+
+    "oil": {
+        "theme": "유가",
+        "leaders": ["흥구석유", "중앙에너비스"],
+        "followers": ["한국석유", "대성에너지"],
+        "related": ["흥아해운", "STX그린로지스"]
+    },
+
+    "shipping": {
+        "theme": "해운",
+        "leaders": ["흥아해운", "STX그린로지스"],
+        "followers": ["대한해운", "팬오션"],
+        "related": ["KSS해운"]
+    },
+
+    "war": {
+        "theme": "전쟁/지정학",
+        "leaders": ["흥아해운"],
+        "followers": ["STX그린로지스"],
+        "related": ["한국석유", "중앙에너비스"]
+    },
+
+    "semiconductor": {
+        "theme": "반도체",
+        "leaders": ["SK하이닉스", "삼성전자"],
+        "followers": ["한미반도체", "주성엔지니어링"],
+        "related": ["테크윙", "디아이"]
+    },
+
+    "ai": {
+        "theme": "AI 반도체",
+        "leaders": ["한미반도체"],
+        "followers": ["테크윙", "디아이"],
+        "related": ["이수페타시스"]
+    },
+
+    "china stimulus": {
+        "theme": "중국 경기부양",
+        "leaders": ["아모레퍼시픽", "LG생활건강"],
+        "followers": ["호텔신라"],
+        "related": ["롯데관광개발"]
+    },
+
+    "container": {
+        "theme": "컨테이너 운임",
+        "leaders": ["흥아해운"],
+        "followers": ["팬오션"],
+        "related": ["STX그린로지스"]
+    },
+
+    "defense": {
+        "theme": "방산",
+        "leaders": ["한화에어로스페이스"],
+        "followers": ["LIG넥스원"],
+        "related": ["현대로템"]
+    },
+
+    "battery": {
+        "theme": "2차전지",
+        "leaders": ["에코프로", "포스코퓨처엠"],
+        "followers": ["엘앤에프"],
+        "related": ["대주전자재료"]
+    }
+
+}
