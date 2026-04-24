d867769a25e78f10fa8de48284fb120b02e36340
ehdud6728-bit <ehdud6728@gmail.com>
2026-04-24 12:16:39 +0900

    Update main7_bugfix_2.py

--- main7_bugfix_2.py
+++ main7_bugfix_2.py
@@ -2,4 +2,4 @@ from runpy import run_path
 from pathlib import Path
 
 if __name__ == "__main__":
-    run_path(str(Path(__file__).resolve().parent / "scanner" / "legacy_main_patched.py"), run_name="__main__")
+    run_path(str(Path(__file__).resolve().parent / "scanner" / "legacy_main_patched_pattern_runtime_complete.py"), run_name="__main__")
